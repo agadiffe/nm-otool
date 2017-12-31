874dcce24045676e7c0f44fc5e34c5bc2b771fee
agadiffe <agadiffe@student.42.fr>
2017-12-31 19:26:22 +0100

    add type symbol + generic x86/x64

--- includes/ft_nm.h
+++ includes/ft_nm.h
@@ -1,7 +1,38 @@
 #ifndef FT_NM_H
 # define FT_NM_H
 
-# define PROT		PROT_READ
+#include "loader.h"
+#include "nlist.h"
+
+# define PROT		PROT_READ | PROT_WRITE
 # define MAP		MAP_PRIVATE
+
+# define X64	1
+# define X86	0
+
+# define NLIST64	((t_nlist64 *)array)
+# define NLIST32	((t_nlist32 *)array)
+
+typedef struct load_command			t_lc;
+typedef struct symtab_command		t_symtab;
+typedef struct mach_header_64		t_header64;
+typedef struct mach_header			t_header32;
+typedef struct fat_header			t_headerfat;
+typedef struct fat_arch				t_arch;
+typedef struct nlist_64				t_nlist64;
+typedef struct nlist				t_nlist32;
+typedef struct segment_command_64	t_seg64;
+typedef struct segment_command		t_seg32;
+typedef struct section_64			t_sect64;
+typedef struct section				t_sect32;
+
+typedef struct				s_ndata
+{
+	t_lc		*lc;
+	uint32_t	ncmds;
+	uint32_t	n_value;
+	uint8_t		n_type;
+	uint8_t		n_sect;
+}							t_ndata;
 
 #endif
