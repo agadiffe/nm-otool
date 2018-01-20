272aaabff6aebff3d6baea781ffa082e448f9b6d
agadiffe <agadiffe@student.42.fr>
2018-01-20 22:57:39 +0100

    fix handle_ar
    not fully fix ...

--- srcs/nm/handle_ar.c
+++ srcs/nm/handle_ar.c
@@ -1,48 +1,49 @@
 #include "ft_nm.h"
 #include "libft.h"
 
-int		get_ar_size(char *name)
+uint32_t	get_ar_name_offset(t_ar *ar)
 {
-	int		x;
-	char	*word;
-
-	word = ft_strchr(name, '/') + 1;
-	x = ft_atoi(word);
-	return (x);
-}
-
-char	*get_ar_name(char *name)
-{
-	int		len;
-
-	len = ft_strlen(ARFMAG);
-	return (ft_strstr(name, ARFMAG) + len);
-}
-
-void	print_ar(char *ptr, uint32_t r_off)
-{
-	t_ar	*ar;
+	int		n;
 	char	*tmp;
 
+	n = 0;
+	if ((tmp = ft_strchr(ar->ar_name, '/')))
+		n = ft_atoi(tmp + 1);
+	return (n);
+}
+
+void		print_ar(char *ptr, uint32_t r_off)
+{
+	t_ar		*ar;
+	char		*tmp;
+	uint32_t	n;
+
 	ar = (void *)ptr + r_off;
-	ft_putstr(get_ar_name(ar->ar_name)); 
+	n = get_ar_name_offset(ar);
+	ft_putstr((void *)ar + sizeof(t_ar));
 	ft_putendl(":");
-	tmp = (void *)ar + sizeof(t_ar) + get_ar_size(ar->ar_name);
+	tmp = (void *)ar + sizeof(t_ar) + n;
 	handle_arch(tmp);
 }
 
-void	handle_ar(char *ptr)
+void		handle_ar(char *ptr)
 {
-	uint32_t	i;
-	uint32_t	size;
 	t_ar		*ar;
 	t_ranlib	*ran;
+	uint32_t	i;
+	uint64_t	size;
+	uint32_t	n;
 
 	ar = (void *)ptr + SARMAG;
-	size = *((int *)((void *)ar + sizeof(t_ar) + get_ar_size(ar->ar_name)));
-	size /= sizeof(t_ranlib);
-	ran = (void *)ar + sizeof(t_ar) + get_ar_size(ar->ar_name) + 4;
+	n = get_ar_name_offset(ar);
+	size = *(uint64_t *)((void *)ar + sizeof(t_ar) + n);
+	size = size / sizeof(t_ranlib);
+	ran = (void *)ar + sizeof(t_ar) + n;
 	i = -1;
 	while (++i < size)
+	{
 		print_ar(ptr, ran[i].ran_off);
+		if (i + 1 != size)
+			ft_putendl("");
+	}
 }
